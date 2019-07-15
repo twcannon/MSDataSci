e734692db2b1684c7aa74d1e8ed4a1b7f025fdce
twcannon <tcannon.mail@gmail.com>
2019-07-15 16:50:09 -0400

    committing changes. lightning! modified:   501/projects/P1_cannon.py

--- 501/projects/P1_cannon.py
+++ 501/projects/P1_cannon.py
@@ -7,23 +7,49 @@ print(sample_data)
 
 T,S,D = np.linalg.svd(sample_data, full_matrices = False)
 
-# print(T)
-print(S*np.identity(9))
-# print(D)
+# print(S)
+Si = 1/S
+Si = Si*np.identity(9)
+print('---------T----------')
+print(T)
+print('---------D----------')
+print(D)
+Dt = np.transpose(D)
+print('---------Dt----------')
+print(Dt)
 
-X = [[0,0,1,1,2],
-    [0,0,0,0,0],
-    [1,0,0,2,0],
-    [0,0,0,1,1],
-    [0,0,0,0,1],
+
+X = [[0,0,0,1,0],
+    [1,0,1,0,0],
     [1,1,0,0,0],
-    [1,0,0,0,1],
-    [1,2,0,0,0],
-    [0,0,1,0,0],
-    [0,0,1,0,0],
-    [1,1,1,1,1],
-    [0,1,0,1,0]]
+    [1,1,1,0,1],
+    [0,0,1,2,0],
+    [0,1,0,1,1],
+    [0,1,0,0,0],
+    [0,0,1,1,0],
+    [0,1,1,0,0],
+    [0,0,0,0,0],
+    [0,0,0,0,0],
+    [0,0,0,0,0]]
 
-Xt = np.transpose(X)
 
-print(Xt)
+print('---------Si----------')
+print(Si)
+print('---------T----------')
+print(T)
+print('----------')
+
+Q = np.matmul(np.matmul(np.transpose(X),T),Si)
+
+print('---------Q----------')
+print(Q)
+
+print(type(T))
+print(type(Q))
+
+plt.scatter(T[:,0],  T[:,1],  color='red',   marker='.', label='Terms')
+plt.scatter(Dt[:,0], Dt[:,1], color='blue',  marker='s', label='Documents')
+plt.scatter(Q[:,0],  Q[:,1],  color='green', marker='x', label='Queries')
+
+plt.legend()
+plt.show()

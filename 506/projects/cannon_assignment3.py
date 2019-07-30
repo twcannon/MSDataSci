a4d165494f43efe6a54f897b36105a96d06b1894
twcannon <tcannon.mail@gmail.com>
2019-07-29 22:53:03 -0400

    more progress modified:   506/projects/cannon_assignment3.py

--- 506/projects/cannon_assignment3.py
+++ 506/projects/cannon_assignment3.py
@@ -1,6 +1,9 @@
 import numpy as np
+# import random
 from scipy import spatial,stats
-import matplotlib.pyplot as plt
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error, r2_score
+from sklearn.preprocessing import PolynomialFeatures
 
 
 
@@ -37,4 +40,30 @@ print('For the x values:')
 print(new_x)
 print('The predicted y values are:')
 print(pred_y)
+print('==========================\n\n')
+
+
 print('==========================')
+print('Q1: b)')
+
+
+
+
+partitions = []
+index = np.arange(0,100)
+np.random.shuffle(index)
+partitions.append(index[0:33])
+partitions.append(index[34:66])
+partitions.append(index[67:100])
+
+for part in partitions:
+    X_test = [X[i] for i in part]
+    Y_test = [Y[i] for i in part]
+    X_train = np.delete(X,[i for i in part])
+    Y_train = np.delete(Y,[i for i in part])
+
+print(np.polyfit(X_train,Y_train,2))
+print(np.polyfit(X_train,Y_train,3))
+print(np.polyfit(X_train,Y_train,4))
+
+

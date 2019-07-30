957bbf37e0ae8642ca4fc5c4667a02fb21552f41
twcannon <tcannon.mail@gmail.com>
2019-07-29 21:10:08 -0400

    finished part a - 506 assignment 3 modified:   506/projects/cannon_assignment3.py

--- 506/projects/cannon_assignment3.py
+++ 506/projects/cannon_assignment3.py
@@ -7,41 +7,34 @@ import matplotlib.pyplot as plt
 
 data = np.genfromtxt('506/data/D3.csv', delimiter = ',')
 
-# print(data)
-
 X = data[:,0]
 Y = data[:,3]
-
-# print(X)
-# print(Y)
 
 avg_x = np.mean(X)
 avg_y = np.mean(Y)
 
-# print(avg_x)
-# print(avg_y)
-
 x_c = X - avg_x
 y_c = Y - avg_y
-
-# print(x_c)
-# print(y_c)
 
 slope = np.dot(x_c,y_c)/np.dot(x_c,x_c)
 
 intercept = avg_y - (slope * avg_x)
+
+new_x = np.array([0.3, 0.5, 0.8])
+
+pred_y = slope*new_x + intercept
+
+
+
+
 print('==========================')
 print('Q1: a)')
-print('---------------------------')
+print('--------------------------')
 print('The linear model is:')
 print('y = {}x + {}'.format(slope,intercept))
+print('--------------------------')
+print('For the x values:')
+print(new_x)
+print('The predicted y values are:')
+print(pred_y)
 print('==========================')
-
-
-
-slope, intercept, r_value, p_value, std_err = stats.linregress(X,Y)
-print(slope)
-print(intercept)
-print(r_value)
-print(p_value)
-print(std_err)

09b9120f7ae8078474fc583c7da9f1128257f4b1
twcannon <tcannon.mail@gmail.com>
2019-07-31 21:10:45 -0400

    figured out Q2 (i think) modified:   506/projects/cannon_assignment4.py

--- 506/projects/cannon_assignment4.py
+++ 506/projects/cannon_assignment4.py
@@ -19,53 +19,55 @@ print(clf.intercept_)
 
 
 
-cur_x = 3 # The algorithm starts at x=3
-rate = 0.01 # Learning rate
-precision = 0.000001 #This tells us when to stop the algorithm
-previous_step_size = 1 #
-max_iters = 10000 # maximum number of iterations
-iters = 0 #iteration counter
-df = lambda x: 2*(x+5) #Gradient of our function
-In [4]:
-while previous_step_size > precision and iters < max_iters:
-    prev_x = cur_x #Store current x value in prev_x
-    cur_x = cur_x - rate * df(prev_x) #Grad descent
-    previous_step_size = abs(cur_x - prev_x) #Change in x
-    iters = iters+1 #iteration count
-    print("Iteration",iters,"\nX value is",cur_x) #Print iterations
+# cur_x = 3 # The algorithm starts at x=3
+# rate = 0.01 # Learning rate
+# precision = 0.000001 #This tells us when to stop the algorithm
+# previous_step_size = 1 #
+# max_iters = 10000 # maximum number of iterations
+# iters = 0 #iteration counter
+# df = lambda x: 2*(x+5) #Gradient of our function
+# In [4]:
+# while previous_step_size > precision and iters < max_iters:
+#     prev_x = cur_x #Store current x value in prev_x
+#     cur_x = cur_x - rate * df(prev_x) #Grad descent
+#     previous_step_size = abs(cur_x - prev_x) #Change in x
+#     iters = iters+1 #iteration count
+#     print("Iteration",iters,"\nX value is",cur_x) #Print iterations
     
-print("The local minimum occurs at", cur_x)
+# print("The local minimum occurs at", cur_x)
 
 
 
 
 
-# dfdx (found by hand)
-dfdx = lambda x,y: 2*x-2*y-10
-
-# dfdy (found by hand)
-dfdy = lambda x,y: 2*y-2*x+4
 
 
-x_0 = 0
-y_0 = 2
-gamma = .01
-i_max = 10
+def grad_desc(gamma):
+    # dfdx (found by hand)
+    dfdx = lambda x,y: 2*x-2*y-10
+    # dfdy (found by hand)
+    dfdy = lambda x,y: 2*y-2*x+4
 
-i = 0
-print('Starting values of x and y: \n x_0 = {}    y_0 = {}\n'.format(x,y))
-while i < i_max:
-    x_prev = x_0
-    y_prev = y_0
-    x_new = x_0 - gamma*N(dfdx.subs(x,x_0).subs(y,y_0)).evalf()
-    y_new = y_0 - gamma*N(dfdy.subs(y,y_0)).subs(x,x_0).evalf()
+    x_0 = 0
+    y_0 = 2
+    i = 0
+    i_max = 10
+    print('\nFor gamma = {}'.format(gamma))
+    print('Starting values of x and y:  \t x_0 = {}    y_0 = {}'.format(x_0,y_0))
+    while i < i_max:
+        x_prev = x_0
+        y_prev = y_0
+        x_0 = x_0 - gamma*dfdx(x_prev,y_prev)
+        y_0 = y_0 - gamma*dfdy(y_prev,y_prev)
 
-    i += 1
-    if i > i_max:
-        break
+        i += 1
+        if i > i_max:
+            break
 
-    x_0 = x_new
-    y_0 = y_new
+        # print('After iteration {}, the values of x and y are: \n'.format(i)) 
+        print('For iteration = {}: \t\t x_{} = {}    y_{} = {}'.format(i,i,x_0,i,y_0))
 
-    print('After iteration {}, the values of x and y are: \n') 
-    print('\t for gamma = {}: \t\t x_{} = {}    y_{} = {}\n'.format(gamma,i,i,x,i,y))
+
+
+grad_desc(0.05)
+grad_desc(0.5)

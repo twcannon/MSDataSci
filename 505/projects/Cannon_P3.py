c41219c389c74859d4fd84638ed685bc322990bb
twcannon <tcannon.mail@gmail.com>
2019-08-01 19:33:40 -0400

    histogram and connectome plot modified:   505/projects/Cannon_P3.py

--- 505/projects/Cannon_P3.py
+++ 505/projects/Cannon_P3.py
@@ -1,5 +1,6 @@
-import numpy as np
 import csv
+import matplotlib.pyplot as plt
+import numpy as np
 
 ids = []
 with open('./505/data/project_three/ids.txt') as id_file:
@@ -8,16 +9,46 @@ with open('./505/data/project_three/ids.txt') as id_file:
         ids.append(line)
 
 iqs = []
-data = []
+matrices = []
+vectors = []
+data_dict = {}
+i = 0
 for id in ids:
+    data_dict[id] = {}
     with open('./505/data/project_three/dataset/{}.txt'.format(id)) as iq_file:
-        iqs.append(iq_file.readline().replace('\n',''))
-    brain_data = np.genfromtxt('./505/data/project_three/dataset/{}.csv'.format(id),delimiter=',') 
-    data.append(brain_data)
-# print(iqs)
+        iq = float(iq_file.readline().replace('\n',''))
+        iqs.append(iq)
+        data_dict[id]['iq'] = iq
+    
+    matrix = np.genfromtxt('./505/data/project_three/dataset/{}.csv'.format(id),delimiter=',') 
+    vector = np.genfromtxt('./505/data/project_three/dataset/{}_vec.csv'.format(id),delimiter=',')
+    matrices.append(matrix)
+    vectors.append(vector)
 
-data = np.array(data)
-# print(data)
+    data_dict[id]['pid'] = i
+    data_dict[id]['matrix'] = matrix
+    data_dict[id]['vector'] = vector
 
+    i += 1
+
+matrices = np.array(matrices)
+vectors = np.array(vectors)
 regions = list(csv.reader(open("./505/data/project_three/Atlas_regions.csv")))
-# print(regions)
+
+# # iqs are not normally distributed??
+# plt.hist(iqs,bins=20)
+# plt.show()
+
+
+
+def graphdata(patientid):
+    iq=data_dict[patientid]['iq']
+    plt.figure(figsize = [10,10])
+    plt.imshow(data_dict[patientid]['matrix'])
+    plt.set_cmap('hot')
+    plt.colorbar()
+    plt.title( f"IQ Score of {iq} ")
+    plt.show()
+
+
+graphdata('NS061')

d920afd8de12695de1483c403cae0abdef3db147
twcannon <tcannon.mail@gmail.com>
2019-09-18 22:16:32 -0400

    DATA510 updates modified:   ../../../.gitignore modified:   .idea/dataSources.xml deleted:    FirstDjangoProject/__pycache__/urls.cpython-37.pyc modified:   FirstDjangoProject/urls.py new file:   main/templates/main/index.html modified:   main/views.py

--- DATA510/projects/FirstDjangoProject/main/views.py
+++ DATA510/projects/FirstDjangoProject/main/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from django.template import loader
+from django.http.response import HttpResponse
+from random import shuffle
+from .models import *
 
-# Create your views here.
+def index(request):
+
+    allStudents = Student.objects.all()
+
+    myList = list(range(1,101))
+    shuffle(myList)
+    print(myList)
+
+    # myDictionary = {'myListKey':myList}
+    myDictionary = {'myListKey':myList,
+                    'allStudentsKey': allStudents}
+
+    template = loader.get_template('main/index.html')
+    # return HttpResponse(template.render(myDictionary,request))
+    return HttpResponse(template.render(myDictionary, request))

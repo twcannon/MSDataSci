e25a0b6dbdf2264c8147a3d00fcc7dd9582ce8ad
twcannon <tcannon.mail@gmail.com>
2019-09-19 16:58:00 -0400

    more work on DATA510 modified:   main/templates/main/index.html modified:   main/views.py

--- DATA510/projects/FirstDjangoProject/main/views.py
+++ DATA510/projects/FirstDjangoProject/main/views.py
@@ -6,15 +6,28 @@ from .models import *
 
 def index(request):
 
-    allStudents = Student.objects.all()
+    # allStudents = Student.objects.all()
+    # allStudents = Student.objects.filter(student_age__gt=25).order_by('student_age')
+    allStudents = Student.objects.all().order_by('student_age')
+    # print(allStudents)
 
     myList = list(range(1,101))
     shuffle(myList)
-    print(myList)
+    # print(myList)
+
+    curRow = Student.objects.filter(first_name="Malcolm")[0]
+    curRow.student_age = 56
+    curRow.save()
+
+
+    if len(Student.objects.filter(first_name="Michael")) == 0:
+        newRow = Student(first_name="Michael",
+                         last_name="L",
+                         student_age=58)
+        newRow.save()
 
     # myDictionary = {'myListKey':myList}
-    myDictionary = {'myListKey':myList,
-                    'allStudentsKey': allStudents}
+    myDictionary = {'myListKey': myList, 'allStudentsKey': allStudents}
 
     template = loader.get_template('main/index.html')
     # return HttpResponse(template.render(myDictionary,request))

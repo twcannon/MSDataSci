c98f5f396e2ab867e46b9196ad32ce0db5b0120a
twcannon <tcannon.mail@gmail.com>
2019-09-24 22:38:54 -0400

    a little more work for MATH550 HW2 modified:   MATH550/homework/HW2.R

--- MATH550/homework/HW2.R
+++ MATH550/homework/HW2.R
@@ -1,5 +1,54 @@
 library(Stat2Data)
 library(MASS)
+
+#Question 2
+bears <- read.table("/home/thomas/git/datascience/MSDataSci/MATH550/data/Bears.csv",header=TRUE,sep=",")
+attach(bears)
+
+#Part A
+plot(ChestGirth, Weight, pch = 16, cex = 1.3, col = "blue", main = "Weight plotted against Chest Girth", xlab = "Chest Girth (inches)", ylab = "Weight (pounds)")
+lm(Weight ~ ChestGirth)
+abline(lm(Weight ~ ChestGirth))
+modelA <- lm(Weight ~ ChestGirth)
+par(mfrow = c(2,2))
+plot(modelA)
+
+#Part B
+ChestGirth2 <- ChestGirth^2
+
+plot(ChestGirth2, Weight, pch = 16, cex = 1.3, col = "blue", main = "Weight plotted against Chest Girth^2", xlab = "Chest Girth^2 (inches^2)", ylab = "Weight (pounds)")
+lm(Weight ~ ChestGirth2)
+abline(lm(Weight ~ ChestGirth2))
+modelB <- lm(Weight ~ ChestGirth2)
+par(mfrow = c(2,2))
+plot(modelB)
+
+#Part C
+logChestGirth <- log(ChestGirth)
+logWeight <- log(Weight)
+
+plot(logChestGirth, logWeight, pch = 16, cex = 1.3, col = "blue", main = "log(Weight) plotted against log(Chest Girth)", xlab = "Chest Girth (inches)", ylab = "Weight (pounds)")
+lm(logWeight ~ logChestGirth)
+abline(lm(logWeight ~ logChestGirth))
+modelC <- lm(logWeight ~ logChestGirth)
+par(mfrow = c(2,2))
+plot(modelC)
+
+#Part D
+#95% confidence intervals on page 24
+round(confint(modelA,level=0.95),3)
+
+predict(modelA,38,interval="prediction",level=0.95)
+
+
+
+
+
+
+
+
+#Question 3
+
 data(PalmBeach)
 palm<-PalmBeach
 
@@ -12,27 +61,24 @@ plot(standres~palm.lm$fitted.values,pch=19,col=12)
 
 leverage <- hatvalues(palm.lm)
 leverage
-plot(leverage~palm.lm$fitted.values,pch=19,col=12)
+plot(leverage,pch=19,col=12)
 
-plot(palm.lm,which=c(1,2,3,4),pch=19,col=10)
+plot(palm.lm)
 
-palm.lm2<-lm(Buchanan~Bush,data=palm[-50])
-plot(Buchanan~Bush,data=palm[-50,-13],col="purple",pch=19)
+
+palm.removed <- palm.removed[- c(13,50),]
+
+log.Buchanan <- log(palm.removed$Buchanan)
+log.Bush <- log(palm.removed$Bush)
+plot(log.Buchanan~log.Bush,col="purple",pch=19)
 abline(palm.lm2,lwd=2)
 
-## ignore this stuff for now
-plot(palm.lm$residuals~palm.lm$fitted.values,pch=19,col=12)
-abline(h=0,lwd=2)
-plot(Buchanan~Bush,data=palm,col="purple",pch=19)
-abline(palm.lm,lwd=2)
-palm.lm2<-lm(Buchanan~Bush,data=palm[-50,-13])
-abline(palm.lm2,lwd=2,lty=2)
-summary(palm.lm)
-summary(palm.lm2)
 
-stu<-studres(palm.lm)
+palm.lm2<-lm(log.Buchanan~log.Bush)
+plot(palm.lm2)
 
-stu[50]
-standres[50]
-standres
+round(confint(palm.lm2,level=0.95),3)
+
+predict(palm.lm2,38,interval="prediction",level=0.95)
+
 

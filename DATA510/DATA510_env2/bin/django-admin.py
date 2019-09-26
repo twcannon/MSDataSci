00edc66c15d3327e186533e5f1702d9017aeaf11
twcannon <tcannon.mail@gmail.com>
2019-09-26 16:07:30 -0400

    added a lot of stuff

---
+++ DATA510/DATA510_env2/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/thomas/git/datascience/MSDataSci/DATA510/DATA510_env2/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()

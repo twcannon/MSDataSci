4d9a258012569f7c30ed3b640f3f5dd631f86675
twcannon <tcannon.mail@gmail.com>
2019-10-08 23:01:03 -0400

    MATH 550 HW3 #5.2 modified:   MATH550/homework/HW3.R

--- MATH550/homework/HW3.R
+++ MATH550/homework/HW3.R
@@ -54,11 +54,31 @@ points(houston$X.Low.income.students[houston$Year==2004],houston$X.Repeating.1st
 abline(model1)
 
 # PART A
-model1 <- lm(X.Repeating.1st.Grade ~ X.Low.income.students, data = houston)
-summary(model1)
+grade.model1 <- lm(X.Repeating.1st.Grade ~ X.Low.income.students, data = houston)
+summary(grade.model1)
 par(mfrow=c(2,2))
-plot(model1)
+plot(grade.model1)
 # The fit appears good with a significant slope.
 
 # PART B
+grade.model2 <- lm(X.Repeating.1st.Grade ~ Year, data = houston)
+summary(grade.model2)
+plot(grade.model2)
+# there is no visually discernible difference between the two populations
+t.test(houston$X.Repeating.1st.Grade~houston$Year)
+# a quick t test also shows that with p value of over .10, there is not a significant difference in the two populations
 
+# PART C
+grade.model3 <- lm(X.Repeating.1st.Grade ~ Year + X.Low.income.students, data = houston)
+summary(grade.model3)
+plot(grade.model3)
+
+grade.model4 <- lm(X.Repeating.1st.Grade ~ X.Low.income.students + Year + X.Low.income.students:Year, data = houston)
+summary(grade.model4)
+plot(grade.model4)
+# There is not much to show for the association between the low-income students and failure rates between the years
+anova(grade.model3,grade.model4)
+anova(grade.model1,grade.model4)
+
+# It is reasonable to not consider the Year in the model, but including low-income is better than not having it. 
+# The year does not influence the model, but there is an association between low-income students and higher grade repitions.

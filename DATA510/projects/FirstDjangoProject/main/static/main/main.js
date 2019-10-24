a3ace02fd2f2e3d4cc14dc3b9466e9e646f0ef08
twcannon <tcannon.mail@gmail.com>
2019-10-24 16:57:14 -0400

    lots of stuff

---
+++ DATA510/projects/FirstDjangoProject/main/static/main/main.js
@@ -0,0 +1,18 @@
+function doSomething() {
+    var myVariable = $("#input_name").val();
+    console.log(myVariable);
+
+    $.ajax(
+        {
+            type: "POST",
+            data: { 
+                myVariableKey: myVariable
+            },
+            success: function (returnedData) {
+                $("#btn_02").text(returnedData['places'][0]['place name'])
+                console.log(returnedData['places'][0]['place name'])       
+            }
+        }
+    );
+
+}
--- DATA510/projects/FirstDjangoProject/main/views.py
+++ DATA510/projects/FirstDjangoProject/main/views.py
@@ -4,31 +4,53 @@ from django.http.response import HttpResponse
 from random import shuffle
 from .models import *
 
+from django.views.decorators.csrf import csrf_exempt
+
+import requests
+
+import json
+
+def render_to_json_response(context, **response_kwargs):
+   data = json.dumps(context)
+   response_kwargs['content_type'] = 'application/json'
+   return HttpResponse(data, **response_kwargs)
+
+
+@csrf_exempt
 def index(request):
 
-    # allStudents = Student.objects.all()
-    # allStudents = Student.objects.filter(student_age__gt=25).order_by('student_age')
-    allStudents = Student.objects.all().order_by('student_age')
-    # print(allStudents)
+    if request.is_ajax():
+        # otherFunction()
+        zipCode = request.POST.get('myVariableKey')
+        r = requests.get('http://api.zippopotam.us/us/' + zipCode)
+        print(str(r))
+        d = r.json()
+        print(d)
+        return render_to_json_response(d)
 
-    myList = list(range(1,101))
-    shuffle(myList)
-    # print(myList)
+    # # allStudents = Student.objects.all()
+    # # allStudents = Student.objects.filter(student_age__gt=25).order_by('student_age')
+    # allStudents = Student.objects.all().order_by('student_age')
+    # # print(allStudents)
 
-    curRow = Student.objects.filter(first_name="Malcolm")[0]
-    curRow.student_age = 56
-    curRow.save()
+    # myList = list(range(1,101))
+    # shuffle(myList)
+    # # print(myList)
+
+    # curRow = Student.objects.filter(first_name="Malcolm")[0]
+    # curRow.student_age = 56
+    # curRow.save()
 
 
-    if len(Student.objects.filter(first_name="Michael")) == 0:
-        newRow = Student(first_name="Michael",
-                         last_name="L",
-                         student_age=58)
-        newRow.save()
+    # if len(Student.objects.filter(first_name="Michael")) == 0:
+    #     newRow = Student(first_name="Michael",
+    #                      last_name="L",
+    #                      student_age=58)
+    #     newRow.save()
 
-    # myDictionary = {'myListKey':myList}
-    myDictionary = {'myListKey': myList, 'allStudentsKey': allStudents}
+    # # myDictionary = {'myListKey':myList}
+    # myDictionary = {'myListKey': myList, 'allStudentsKey': allStudents}
 
     template = loader.get_template('main/index.html')
     # return HttpResponse(template.render(myDictionary,request))
-    return HttpResponse(template.render(myDictionary, request))
+    return HttpResponse(template.render({}, request))

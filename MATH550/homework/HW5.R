d8b8cbfee725ee8185e6752077cd532f879dbf48
twcannon <tcannon.mail@gmail.com>
2019-11-13 17:29:53 -0500

    MATH550 HW5 Continued modified:   MATH550/homework/HW5.R

--- MATH550/homework/HW5.R
+++ MATH550/homework/HW5.R
@@ -84,3 +84,125 @@ abline(h=0,lwd=2)
 # instead of just a power transform. Also, there are a few points that look like they might be 
 # providing much more leverage than others and may need to be addressed.
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+golf <- read.csv("/home/thomas/git/datascience/MSDataSci/MATH550/data/pgatour2006.csv",header=TRUE)
+# Removing unneeded columns
+golf <- subset(golf, select=-c(Name,TigerWoods,AveDrivingDistance,BounceBack))
+attach(golf)
+golf
+pairs(PrizeMoney~., data = golf)
+
+model_golf <- lm(PrizeMoney ~., data=golf)
+summary(model_golf)
+# Running a full summary of a linear model now doesnot give us the best R-squared value.
+# Let's try a transform...
+
+library(car)
+par(mfrow = c(1,1))
+invResults <- invResPlot(model_golf)
+# The Inverse response plot picked out a visually appropriate lambda-hat of about 0.04
+# to use as an exponent int he power transform for PrizeMoney
+
+# assigning lambda-hat to a variable
+lambdaHat <- invResults$lambda[1]
+p1 <- powerTransform(PrizeMoney ~.,data=golf)
+
+# Power transforming PrizeMoney
+golf$PrizeMoney <- golf$PrizeMoney^lambdaHat
+golf
+attach(golf)
+pairs(PrizeMoney~., data = golf)
+# The new pairs plot looks much better. Now, a rough linear relationship can be seen
+# between PrizeMoney and the predictors. 
+model_golf <- lm(PrizeMoney ~., data=golf)
+summary(model_golf)
+# The adjusted R-squared value for the new power-transformed model is a lot better at 0.5441.
+# Now, let's try a Log Transformation on Prize Money
+
+# Log transforming PrizeMoney
+golf$PrizeMoney <- log(golf$PrizeMoney)
+golf
+attach(golf)
+pairs(PrizeMoney~., data = golf)
+# The new pairs plot looks much better. Now, a rough linear relationship can be seen
+# between PrizeMoney and the predictors. 
+model_golf <- lm(PrizeMoney ~., data=golf)
+summary(model_golf)
+# The adjusted R-squared value for the new power-transformed model is a lot better.
+
+
+# In the problem summary (and as can be seen in the new pairs plot),
+# it alluded that there were several predictors that coud be correlated.
+# because of this, Iwill perform backwards step AIC and BIC to reduce the
+# number of predictors and potential multicollinearity
+n <- length(model_golf$residuals)
+backAIC  <- step(model_golf,direction="backward", data=golf)
+backBIC  <- step(model_golf,direction="backward", data=golf, k=log(n))
+# The step model results driven by AIC give us back TKN, TS, and COD as the best predictors
+# For BIC, I am given TS and COD
+# Since there are already so few terms, the BIC approach might not be completely necessary
+# It might be better to evaluate the models for each before making a decision on the best
+
+# AIC
+model_golf <- lm(PrizeMoney~SandSaves+Scrambling+GIR+BirdieConversion, data=golf)
+summary(model_golf)
+# BIC
+model_golf <- lm(PrizeMoney~Scrambling+GIR+BirdieConversion, data=golf)
+summary(model_golf)
+pairs(PrizeMoney~SandSaves+Scrambling+GIR+BirdieConversion, data = golf)
+# While the AIC has an additional term, the R-squared is slightly higher than using BIC
+# When I go back to look at the problem statement, it does not specifically mention
+# any previously understood relationship between TKN, TS, and COD.
+# This suggests that perhaps the span of these three predictors will still be appropriate 
+# for a model. So, I will beusing the predictors given to us by the AIC step results. 
+
+
+
+library(alr4)
+par(mfrow = c(2,2))
+mmp(model_golf)
+mmp(model_golf,SandSaves)
+mmp(model_golf,Scrambling)
+mmp(model_golf,GIR)
+mmp(model_golf,BirdieConversion)
+# the Marginal Model plots for the model and the individual predictors
+# look good. The data tracks well with the proposed model
+
+
+StanRes <- rstandard(model_golf)
+par(mfrow = c(2,2))
+plot(SandSaves,StanRes, ylab="Standardized Residuals",pch=19,col=10)
+abline(h=0,lwd=2)
+plot(Scrambling,StanRes, ylab="Standardized Residuals",pch=19,col=10)
+abline(h=0,lwd=2)
+plot(GIR,StanRes, ylab="Standardized Residuals",pch=19,col=10)
+abline(h=0,lwd=2)
+plot(BirdieConversion,StanRes, ylab="Standardized Residuals",pch=19,col=10)
+abline(h=0,lwd=2)
+# The standard residuals also are looking nice and flat.
+
+# If I had to investigate the model further, I would look into a log transform of O2UP
+# instead of just a power transform. Also, there are a few points that look like they might be 
+# providing much more leverage than others and may need to be addressed.

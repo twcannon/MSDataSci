e4766d5660e7870543b22215c47c6197ab9c6811
twcannon <tcannon.mail@gmail.com>
2020-04-29 17:55:17 +0000

    Data Final Qs 2,3,4

--- DATA534/final/DATA510_Question02.py
+++ DATA534/final/DATA510_Question02.py
@@ -1,4 +1,29 @@
 
+
+# EXAMPLE OUTPUT
+
+'''######################
+            .
+            .
+            .
+            .
+Predicted is: 6. Actual is 6
+Predicted is: 7. Actual is 6
+Predicted is: 6. Actual is 5
+Predicted is: 6. Actual is 5
+Predicted is: 6. Actual is 6
+Predicted is: 6. Actual is 5
+Predicted is: 6. Actual is 5
+Predicted is: 6. Actual is 6
+Predicted is: 7. Actual is 5
+Predicted is: 6. Actual is 6
+Predicted is: 6. Actual is 6
+Predicted is: 7. Actual is 6
+413/980 Correct
+'''######################
+
+
+
 import numpy as np
 from sklearn.model_selection import train_test_split
 import tensorflow.compat.v1 as tf
@@ -7,13 +32,19 @@ tf.disable_v2_behavior()
 my_data = np.genfromtxt('winequality-white.csv', delimiter=';', skip_header=1)
 np.random.shuffle(my_data)
 
-x, y = my_data[:,:11], my_data[:,11:12]
-training_x, testing_x, training_y, testing_y = train_test_split(x, y, test_size=0.2)
+iterations = 10000
+learning_rate = .000005
 
-training_y_one_hot = np.zeros((training_y.shape[0], 7), dtype=np.int)
 
-for idx, el in enumerate(training_y):
-    training_y_one_hot[idx][int(el)-3] = 1
+def main():
+
+    x, y = my_data[:,:11], my_data[:,11:12]
+    training_x, testing_x, training_y, testing_y = train_test_split(x, y, test_size=0.2)
+
+    training_y_one_hot = np.zeros((training_y.shape[0], 7), dtype=np.int)
+
+    for idx, el in enumerate(training_y):
+        training_y_one_hot[idx][int(el)-3] = 1
 
     X = tf.placeholder(dtype=tf.float32)
     Y = tf.placeholder(dtype=tf.float32)
@@ -37,20 +68,29 @@ for idx, el in enumerate(training_y):
 
     loss = tf.reduce_mean(loss)
 
-    optimizer = tf.train.GradientDescentOptimizer(.00001).minimize(loss)
+    optimizer = tf.train.GradientDescentOptimizer(learning_rate).minimize(loss)
     init = tf.global_variables_initializer()
 
     with tf.Session() as sess:
-      sess.run(init)
+        sess.run(init)
 
-      for numOfIterations in range(300):
-        sess.run(optimizer, feed_dict={X: training_x, Y: training_y_one_hot})
-        c = sess.run(loss, feed_dict={X: training_x, Y: training_y_one_hot})
-        print(c)
-        print(numOfIterations, "/100")
+        for numOfIterations in range(iterations):
+            sess.run(optimizer, feed_dict={X: training_x, Y: training_y_one_hot})
+            c = sess.run(loss, feed_dict={X: training_x, Y: training_y_one_hot})
+            print('iteration',numOfIterations, "/"+str(iterations),'| loss',c)
 
-      results = sess.run(y_hat_softmax, feed_dict={X: testing_x})
-      index_of_highest_predicted = np.argmax(results, axis=1)
+        results = sess.run(y_hat_softmax, feed_dict={X: testing_x})
+        index_of_highest_predicted = np.argmax(results, axis=1)
 
-      for idx in range(len(testing_y)):
-          print("Actual is " + str(testing_y[idx]) + ". Predicted is: " + str(index_of_highest_predicted[idx] + 3))
+        num_correct = 0
+        for idx in range(len(testing_y)):
+            pred = int(index_of_highest_predicted[idx] + 3)
+            act  = int(testing_y[idx][0])
+            if pred == act:
+                num_correct +=1
+            print("Predicted is: " + str(pred) + ". Actual is " + str(act))
+        print(str(num_correct)+'/'+str(len(testing_y))+' Correct')
+
+
+if __name__ == "__main__":
+    main()
